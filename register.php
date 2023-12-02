db0d730b168346c4fcdc252b3e13863fba67bb74
vmmlmanasa <manasa_csd@dsrkrec.edu.in>
2023-12-02 06:19:16 +0530

    register

--- register.php
+++ register.php
@@ -2,16 +2,17 @@
 $host = "localhost";
 $username = "root";
 $password = "";
-$database = "feedback";
+$database = "fooddelivery";
 $conn = new mysqli($host, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
-    $email = $_POST["email"];
+    $name = $_POST["name"];
+    $mobile = $_POST["mobile"];
     $password = $_POST["password"];
-    $sql = "INSERT INTO register (username, email, password) VALUES ('$username', '$email', '$password')";
+    $sql = "INSERT INTO users (`uid`, `name`, `mobile` , `password`) VALUES ('$username', '$name','$mobile', '$password')";
 
     if ($conn->query($sql) === TRUE) {
         echo "Registration successful";
